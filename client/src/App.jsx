5fc34c3e03e742652acb47f4e03222a5e1b1226e
nicmackenzie <nicmackenzie@live.com>
2023-09-05 09:10:01 +0300

    client routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './features/authentication/Login';
+import SignUp from './features/authentication/SignUp';
+import Suggestions from './features/suggestions/Suggestions';
+import Suggestion from './features/suggestions/Suggestion';
+
 function App() {
   return (
-    <>
-      <h1>Hi front the frontend</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/" element={<Suggestions />} />
+        <Route path="/suggestions/:id" element={<Suggestion />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
