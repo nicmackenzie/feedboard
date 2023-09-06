2262e7818871d729cda36da118c1f96b01b8c560
nicmackenzie <nicmackenzie@live.com>
2023-09-06 22:01:02 +0300

    redefine relations

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,2 +1,5 @@
 class SuggestionsController < ApplicationController
+    def index
+        render json: Suggestion.all
+    end
 end
--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,4 +1,5 @@
 class Suggestion < ApplicationRecord
+    belongs_to :category
+    belongs_to :user
     has_many :comments
-    has_many :categories
 end
