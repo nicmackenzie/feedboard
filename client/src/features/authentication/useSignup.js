1f8ed63b616e4f4224fe79a3b7c7bb0e1527a2cf
nicmackenzie <nicmackenzie@live.com>
2023-09-06 19:49:09 +0300

    setup user context functionality

--- client/src/features/authentication/useSignup.js
+++ client/src/features/authentication/useSignup.js
@@ -1,12 +1,15 @@
 import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 import { signup as signupApi } from '../../services/auth';
+import { useUser } from '../../context/user-context';
 
 export function useSignup() {
   const navigate = useNavigate();
+  const { setUser } = useUser();
   const { isLoading: isSigninIn, mutate: signup } = useMutation({
     mutationFn: userDetails => signupApi(userDetails),
     onSuccess: data => {
+      setUser(data);
       navigate('/', { replace: true });
     },
   });
