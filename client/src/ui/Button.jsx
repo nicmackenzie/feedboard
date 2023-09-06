8b97c08521caf95f19283869c7cc38bbbfc3b002
nicmackenzie <nicmackenzie@live.com>
2023-09-06 21:28:34 +0300

    Button styles

--- client/src/ui/Button.jsx
+++ client/src/ui/Button.jsx
@@ -1,10 +1,6 @@
-function Button({ children, type, isLoading }) {
+function Button({ children, type, isLoading, disabled }) {
   return (
-    <button
-      type={type}
-      disabled={isLoading}
-      className="rounded-md bg-clr-purple px-3 py-2 text-sm font-semibold text-white shadow-sm transition-colors hover:bg-clr-purple hover:opacity-80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-    >
+    <button type={type} disabled={disabled || isLoading} className="btn">
       {children}
     </button>
   );
