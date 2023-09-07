0ad18358e8ef85dda01842d414a82b9819122dc6
nicmackenzie <nicmackenzie@live.com>
2023-09-07 14:40:43 +0300

    add login card

--- client/src/features/suggestions/LeftSide.jsx
+++ client/src/features/suggestions/LeftSide.jsx
@@ -1,6 +1,9 @@
 import { FaSpinner } from 'react-icons/fa';
 import { useGetCategories } from './useGetCategories';
+import { useUser } from '../../context/user-context';
+import { Link } from 'react-router-dom';
 function LeftSide() {
+  const { loggedInUser, setUser } = useUser();
   const { isLoadingCategories, categories } = useGetCategories();
   return (
     <div className="space-y-6">
@@ -18,6 +21,7 @@ function LeftSide() {
           ))
         )}
       </div>
+      {!loggedInUser && <Login onFakeLogin={setUser} />}
     </div>
   );
 }
@@ -35,6 +39,37 @@ export function Category({ category }) {
     <span className="inline-block px-4 py-2 bg-violet-100 transition-colors hover:bg-violet-200 text-clr-blue-primary font-semibold cursor-pointer rounded-md text-sm ">
       {category}
     </span>
+  );
+}
+
+function Login({ onFakeLogin }) {
+  const fakeUser = {
+    id: 1,
+    user_name: 'Fake User',
+    avatar_url: 'https://i.pravatar.cc/48?u=499476',
+    email: 'fake@test.com',
+  };
+  return (
+    <div className="bg-clr-white-accent shadow-md rounded-lg p-4 space-y-2">
+      <p className="text-sm text-clr-gray-accent">
+        You must be signed in to add suggestion,upvote or comment
+      </p>
+
+      <div className="flex items-center justify-between">
+        <Link
+          to="/login"
+          className="text-clr-blue-primary font-semibold text-sm"
+        >
+          Log in
+        </Link>
+        <button
+          className="text-clr-blue-secondary font-semibold text-sm"
+          onClick={() => onFakeLogin(fakeUser)}
+        >
+          Fake Login
+        </button>
+      </div>
+    </div>
   );
 }
 
