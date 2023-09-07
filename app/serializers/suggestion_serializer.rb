831aec97e21db6bf585feb2d81f6a30b3d4a8a24
nicmackenzie <nicmackenzie@live.com>
2023-09-07 08:20:23 +0300

    show comments

--- app/serializers/suggestion_serializer.rb
+++ app/serializers/suggestion_serializer.rb
@@ -1,6 +1,8 @@
 class SuggestionSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :upvotes, :category
   
+  has_many :comments
+
   def category
     object.category.category
   end
