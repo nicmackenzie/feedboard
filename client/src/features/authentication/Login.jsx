a098363bffe243ad1bbbb0e4764fbd596e3d3ac5
Mike Makol <mikemakol45@gmail.com>
2023-09-09 11:54:13 +0300

    authenticate login

--- client/src/features/authentication/Login.jsx
+++ client/src/features/authentication/Login.jsx
@@ -1,10 +1,43 @@
 import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
 import Button from '../../ui/Button';
 import FormControl from '../../ui/FormControl';
+import { validateLogin } from './validations';
+import { useLogin } from './useLogin';
+
+const uid = function () {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+};
 
 function Login() {
+  const { isSigninIn, login } = useLogin();
+  function onSubmit(values) {
+    const formValues = {
+      email: values.email,
+      password: values.password,
+      avatar_url: `https://i.pravatar.cc/48?u=${uid()}`,
+    };
+    login(formValues, {
+      onError: error => {
+        console.log(typeof error.message);
+      },
+    });
+  }
+
+  const { handleChange, handleBlur, handleSubmit, values, errors, touched } =
+    useFormik({
+      initialValues: {
+        email: '',
+        password: '',
+      },
+      validate: validateLogin,
+      onSubmit,
+    });
   return (
-    <form className="max-w-md bg-white shadow-sm rounded-sm p-4 mx-auto mt-12 space-y-4">
+    <form 
+      className="max-w-md bg-white shadow-sm rounded-sm p-4 mx-auto mt-12 space-y-4"
+      onSubmit={handleSubmit}
+    >
     <h1 className="text-clr-purple text-center font-bold text-2xl">
       Enter your information to Log in
     </h1>
@@ -14,14 +47,24 @@ function Login() {
       placeholder="eg jdoe@email.com"
       id="email"
       name="email"
+      value={values.email}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      error={touched.email && errors.email}
     />
     <FormControl
       label="Password"
       type="password"
       id="password"
       name="password"
+      value={values.password}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      error={touched.password && errors.password}
     />
-    <Button>Log In</Button>
+    <Button type="submit" isLoading={isSigninIn}>
+      Log In
+    </Button>
     <p className="text-clr-gray-accent text-sm text-center font-semibold">
       Need an account?{' '}
       <span>
